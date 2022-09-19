714b1789ab0af1b6b435fdb617eda62b2b1babd6
Lanrewest <lanrewese@yahoo.com>
2022-09-19 14:49:26 -0700

    task5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,35 +3,35 @@
 
 /**
 * rev_string - Reverses a string
-* @s: string to be reversed
+* @k: string to be reversed
 *
 * Return: nothing
 */
 
-void rev_string(char *s)
+void rev_string(char *k)
 {
-	int i, tmp, len = _strlen(s);
+	int i, tmp, len = _strle8n(k);
 
 	for (i = 0; i < len / 2; i++)
 	{
-		tmp = *(s + i);
-		*(s + i) = *(s + len - i - 1);
-		*(s + len - i - 1) = tmp;
+		tmp = *(k + i);
+		*(k + i) = *(k + len - i - 1);
+		*(k + len - i - 1) = tmp;
 	}
 }
 
 /**
 * _strlen - returns the length of a string
-* @s: string
+* @k: string
 *
 * Return: the length of the given string
 */
 
-int _strlen(char *s)
+int _strlen(char *k)
 {
 	int len = 0;
 
-	while (*(s + len) != '\0')
+	while (*(k + len) != '\0')
 		len++;
 
 	return (len);
