e62c609ac17b59ab4b9f8570f052c07d0f2f9eb7
Lanrewest <lanrewese@yahoo.com>
2022-09-19 14:36:39 -0700

    task3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,19 +3,19 @@
 
 /**
  *_puts - prints a string
- * @str: string to print
+ * @strg: string to print
  *
  * Description: prints a string
  * On success: returns no error
  */
 
-void _puts(char *str)
+void _puts(char *strg)
 {
 	int i = 0;
 
-	while (*(str + i) != '\0')
+	while (*(strg + i) != '\0')
 	{
-		putchar(*(str + i));
+		putchar(*(strg + i));
 		i++;
 	}
 	putchar(10);
