41ea6c58398a6ef17769ba127e4702ffa1308491
Lanrewest <lanrewese@yahoo.com>
2022-09-19 14:33:04 -0700

    task2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,15 +2,15 @@
 
 /**
  *_strlen - returns the length of a string
- * @s: string
+ * @g: string
  *Return: returns length as integer;
  */
 
-int _strlen(char *s)
+int _strlen(char *g)
 {
 	int len = 0;
 
-	while (*(s + len) != '\0')
+	while (*(g + len) != '\0')
 		len++;
 
 	return (len);
