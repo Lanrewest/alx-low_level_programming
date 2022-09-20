f6824c3d34ad38ebbd7eaa1aaeaa6388fd5cbbba
Lanrewest <lanrewese@yahoo.com>
2022-09-19 21:57:45 -0700

    taskkk

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,24 +2,24 @@
 
 /**
 * _atoi - converts a string to an int
-* @s: the string to be changed
+* @t: the string to be changed
 *
 * Return: the converted int
 */
 
-int _atoi(char *s)
+int _atoi(char *t)
 {
 	int sign = 1;
 	unsigned int num = 0;
 
 	do {
-		if (*s == '-')
+		if (*t == '-')
 			sign *= -1;
-		else if (*s >= '0' && *s <= '9')
-			num = num * 10 + (*s - '0');
+		else if (*t >= '0' && *t <= '9')
+			num = num * 10 + (*t - '0');
 		else if (num > 0)
 			break;
-	} while (*s++);
+	} while (*t++);
 
 	return (num * sign);
 }
