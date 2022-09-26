9001d0c267fd8957adba713bdc81b7f91d381f72
Lanrewest <lanrewese@yahoo.com>
2022-09-26 13:15:06 -0700

    task1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,20 +2,19 @@
  
 /**
  * memcpy - copies memory area from src to dest
- *  @dest: destination
- *  @src: source
- *   @n: max bytes to use
- *   Return: dest
+ * @dest: destination
+ * @src: source
+ * @n: max bytes to use
+ * Return: dest
  */
+char *_memcpy (char *dest, char *src, unsigned int n)
 
-char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int i;
-for (i=0; n>0; i++,n--)
+for(i = 0; n > 0; i++, n--)
 {
-dest [i]= src[i];
+	dest[i] = src[i];
 }
-return (dest);
-
+return(dest);
 
 }
