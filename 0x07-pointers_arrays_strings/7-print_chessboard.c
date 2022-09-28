4c18b954006a5fe9f3b093c93a1d27afb3371069
Lanrewest <lanrewese@yahoo.com>
2022-09-27 17:16:24 -0700

    task68

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,25 +1,21 @@
-#include "main.h" 
-  
- /** 
-   * print_chessboard - print the chessboard, 2d array 
-    * @a: 2d array of chars 
-   * Return: void 
-  */ 
-  
- void print_chessboard(char (*a)[8]) 
- { 
- /*variables*/ 
- int n, j; 
- n = 0; 
- while (n < 8) /*repeat number*/ 
- { 
- j = 0; 
- while (j < 8) 
- { 
- _putchar (a[n][j]); 
- j++; 
- } 
- _putchar ('\n'); /*print new line*/ 
- n++; 
- } 
- }
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: pointer to pieces to print
+ *
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
