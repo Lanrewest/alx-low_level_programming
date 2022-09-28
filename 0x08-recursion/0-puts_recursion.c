746491cadc078be1264f4e2ac852234e387c6413
Lanrewest <lanrewese@yahoo.com>
2022-09-28 14:09:10 -0700

    task0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,9 +2,7 @@
  * File: 0-puts_recursion.c                                                                                                             
  * Auth: sam                                                                                                                            
  */                                                                                                                                     
-                                                                                                                                        
 #include "main.h"                                                                                                                       
-                                                                                                                                        
 /**                                                                                                                                     
  * _puts_recursion - Prints a string, followed by a new line.                                                                           
  * @s: The string to be printed.                                                                                                        
@@ -16,7 +14,6 @@ void _puts_recursion(char *s)
                 _putchar(*s);                                                                                                           
                 _puts_recursion(s + 1);                                                                                                 
         }                                                                                                                               
-                                                                                                                                        
         else                                                                                                                            
                 _putchar('\n');                                                                                                         
 }
